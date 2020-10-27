acda74b080fb9af52c79a8293e9817f0ecc0ef8c
IraMorokh <morokhovskaya2+1@gmail.com>
2020-10-27 13:12:24 +0200

    Solution for Homework # 42

--- selenium_form.js
+++ selenium_form.js
@@ -2,5 +2,34 @@
 
 // Please automate next test cases:
 // 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
+        // it('Heroku login test', () => {
+        //     cy.visit('https://the-internet.herokuapp.com/login');
+
+        //     cy.get('[name="username"]').type('tomsmith');
+        //     cy.get('[name="password"]').type('SuperSecretPassword!');
+        //     cy.get('[type="submit"] .fa.fa-2x.fa-sign-in').click();
+
+        //     cy.get('div #flash.flash.success').contains('You logged into a secure area!').should('exist');
+        // });
 // 2. Login with invalid creds and check validation error
+        // it('Heroku invalid login test', () => {
+        //     cy.visit('https://the-internet.herokuapp.com/login');
+
+        //     cy.get('[name="username"]').type('tomsmith56');
+        //     cy.get('[name="password"]').type('SuperSecretPassword!');
+        //     cy.get('[type="submit"] .fa.fa-2x.fa-sign-in').click();
+
+        //     cy.get('.flash.error').contains('Your username is invalid!').should('exist');
+        // });
 // 3. Logout from app and assert you successfully logged out
+        // it('Heroku logout test', () => {
+        //     cy.visit('https://the-internet.herokuapp.com/login');
+
+        //     cy.get('[name="username"]').type('tomsmith');
+        //     cy.get('[name="password"]').type('SuperSecretPassword!');
+        //     cy.get('[type="submit"] .fa.fa-2x.fa-sign-in').click();
+
+        //     cy.get('a.button.secondary.radius').click();
+
+        //     cy.get('div #flash.flash.success').contains('You logged out of the secure area!').should('exist');
+        // });
